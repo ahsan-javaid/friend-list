2447ebc42c744b3e2cdd7153e09a168ba9221d78
Ahsan Javaid <ahsan.javaid@onebyte.biz>
2017-06-12 08:20:51 +0500

    change sendgrid key

--- config/auth.js
+++ config/auth.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     'sendgrid':{
         auth: {
-            api_key: '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'
+            api_key: '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'
         }
     }
 };
--- config/mailer.js
+++ config/mailer.js
@@ -1,6 +1,7 @@
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
 var options = require('./auth').sendgrid;
+console.log(options);
 var mailer = nodemailer.createTransport(sgTransport(options));
 var emailSender = {};
 emailSender.sendEmail = function (user) {
@@ -9,9 +10,10 @@ emailSender.sendEmail = function (user) {
         from: 'do-not-reply@friendlist.com',
         to: user.email,
         subject: 'Welcome '+user.name, // Subject line
-        text: 'Hi, welcome ' + user.name+'\n\n\n Regards, Friend List'
+        text: 'Hi, welcome ' + user.name
     };
     mailer.sendMail(email,function(e,r){
+        console.log(e,r);
     });
 }
 module.exports = emailSender;
